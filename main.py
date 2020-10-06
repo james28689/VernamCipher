44fa9badac60b60263ab1c1165487c2e5fa2d0e6
james28689 <james.watling@gmail.com>
2020-10-06 18:25:09 +0100

    Removed "something" print statement

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-import string, random
+import string
+import random
 from functions import xor
 
-def vernam(plaintext, key):
-	binary_plaintext = ["{0:08b}".format(ord(letter)).replace(" ", "") for letter in plaintext]
-	binary_key = ["{0:08b}".format(ord(letter)).replace(" ", "") for letter in key]
-	all_binary = [[binary_plaintext[i], binary_key[i]] for i in range(len(binary_plaintext))]
-	binary_cipher = ["".join([str(xor(couple[0][x], couple[1][x])) for x in range(len(couple[0]))]) for couple in all_binary]
-	plaintext_cipher = "".join([chr(int(letter, 2)) for letter in binary_cipher])
 
-	return(plaintext_cipher)
+def vernam(plaintext, key):
+    binary_plaintext = ["{0:08b}".format(
+        ord(letter)).replace(" ", "") for letter in plaintext]
+    binary_key = ["{0:08b}".format(
+        ord(letter)).replace(" ", "") for letter in key]
+    all_binary = [[binary_plaintext[i], binary_key[i]]
+                  for i in range(len(binary_plaintext))]
+    binary_cipher = ["".join([str(xor(couple[0][x], couple[1][x]))
+                              for x in range(len(couple[0]))]) for couple in all_binary]
+    plaintext_cipher = "".join([chr(int(letter, 2))
+                                for letter in binary_cipher])
+
+    return(plaintext_cipher)
+
 
 plaintext = input("Original Text: ")
-key = [random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for i in range(len(plaintext))]
+key = [random.choice(string.ascii_lowercase + string.ascii_uppercase +
+                     string.digits) for i in range(len(plaintext))]
 
 ciphertext = vernam(plaintext, key)
 print(ciphertext)
 print([letter for letter in ciphertext])
 print("".join(key))
 print(vernam(ciphertext, key))
-print("something")
